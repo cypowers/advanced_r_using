b5902e997433dd1b6c804a0b232f79b25e562542
cypowers <devin17@knou.ac.kr>
2018-08-27 14:14:15 +0900

    스크립트와 교재 데이터 추가

---
+++ scripts/ex1-1.r
@@ -0,0 +1,20 @@
+library(dplyr)
+library(xlsx)
+
+data1 = read.table("Data/1-ex1.txt", header = T)
+data1
+data1 = read.xlsx("Data/1-ex1.xlsx", 1)
+data1
+
+data1.summarise <- data1 %>% group_by(SEX) %>% summarise(n=n()
+                             , dose_avr=mean(DOSE)
+                             , dose_sd=sd(DOSE)
+                             , bp_avr=mean(BP)
+                             , bp_sd=sd(BP)
+                             , age_avr=mean(AGE)
+                             , age_sd=sd(AGE))
+data1.summarise
+
+library(ggplot2)
+kk = data1 %>% select(SEX, DOSE)
+boxplot(kk)
